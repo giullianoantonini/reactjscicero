3ed22e1cf99bde1a3363e764b8c809683d6e8ac1
Giulliano Antonini <giullianoantonini@gmail.com>
2024-11-05 19:59:09 -0300

    Aula 02: antes da limpeza do código

--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 function App() {
   const welcome = <h1>Bem vindo ao curso!</h1>;
   const gostando = true;
+  const numero = 2;
+  const cursos = ["HTML", "CSS", "JS"];
+
   // Continuamos daqui amanhã
 
   return (
@@ -13,8 +16,17 @@ function App() {
         <h1>Aula 01</h1>
         {welcome}
         <h2>Introdução ao React</h2>
+        {gostando ? "Estou  curtindo" : <h3>NÃO CURTI</h3>}
+        <p className={numero % 2 ? "impar" : "par"}>{numero}</p>
+        {gostando && (
+          <div>
+            <h3>Estou curtindo muito</h3>
+          </div>
+        )}
+        {cursos.map((curso) => {
+          return <p>{curso}</p>;
+        })}
       </div>
-      {gostando}
     </>
   );
 }
